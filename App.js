eb0a5c1c1433c98bbaccc5f636b6d5bf5a808a10
Matthew Nilsen <mnilsen50@gmail.com>
2021-02-05 13:55:46 -0500

    MessageModel & update functions

--- App.js
+++ App.js
@@ -2,7 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {MessageModel} from './src/models/MessageModel'
+import {db} from './firebase'
+
+
 export default function App() {
+  let mm = new MessageModel('Test message')
+  mm.saveToFirebase()
+
+  var msgRef = db.ref('messages/');
+  msgRef.on('child_added', (data) => {
+    console.log('msg added')
+    // addCommentElement(postElement, data.key, data.val().text, data.val().author);
+  });
+
+  msgRef.on('child_changed', (data) => {
+    console.log('msg changed')
+    // setCommentValues(postElement, data.key, data.val().text, data.val().author);
+  });
+
+  msgRef.on('child_removed', (data) => {
+    console.log('msg deleted')
+    // deleteComment(postElement, data.key);
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ src/models/MessageModel.js
@@ -0,0 +1,21 @@
+import {db} from '../../firebase'
+
+export class MessageModel {
+    constructor(msg) {
+        this.msg = msg
+    }
+
+    toJSON() {
+        let ts = Date.now();
+        return {
+            timestamp: ts,
+            text: this.msg
+        }
+    }
+
+    saveToFirebase() {
+        db
+        .ref('/messages')
+        .push(this.toJSON(this.msg));
+    }
+}
