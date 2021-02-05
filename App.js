60e5de5651ab901035c0c916ff7454b84f12ed2a
andy <alchemistlizd@gmail.com>
2021-02-04 21:52:31 -0500

    add button and post component

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [post, setPost] = useState("Lorem");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text> CS 4261 - Assignment 1.0 </Text> */}
+      <View style={{ width: "80%", height: "40%" }}>
+        <Text style={{ textAlign: "center" }}>{post}</Text>
+      </View>
+      <Button title="Get A Random Post" onPress={() => handlePress(setPost)} />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const handlePress = (setPost) => {
+  console.log("button pressed");
+  setPost("lorem " + Math.random());
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
